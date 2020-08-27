d1a95703b27864025475ef2a2257d212aa357513
Motokazu Sekine <cheebow@gmail.com>
2020-08-27 18:21:15 +0900

    最初のコミット

---
+++ MobileViewer/MobileViewer/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MobileViewer
+//
+//  Created by CHEEBOW on 2020/08/27.
+//  Copyright © 2020 CHEEBOW. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ MobileViewer/MobileViewer/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  MobileViewer
+//
+//  Created by CHEEBOW on 2020/08/27.
+//  Copyright © 2020 CHEEBOW. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    let siteURL = "https://backnumber.rittor-music.co.jp"
+
+    private var webView: WKWebView! = {
+       let configuration = WKWebViewConfiguration()
+        let webview = WKWebView(frame: .zero, configuration: configuration)
+        webview.backgroundColor = UIColor.white
+        return webview
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+
+        guard let url = URL(string: siteURL) else {
+            return
+        }
+        let request = URLRequest(url: url)
+
+        webView.load(request)
+    }
+
+    override func viewDidLayoutSubviews() {
+        webView.frame = view.bounds
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, preferences: WKWebpagePreferences, decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void) {
+        preferences.preferredContentMode = .mobile
+        decisionHandler(.allow, preferences)
+    }
+}
